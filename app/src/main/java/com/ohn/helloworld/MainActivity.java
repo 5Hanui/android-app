0eee9308b1d5ad835e34b01c06b74010e6001877
5Hanui <gkdud644@naver.com>
2021-07-05 17:12:24 +0900

    레이아웃 연습

--- app/src/main/java/com/ohn/helloworld/MainActivity.java
+++ app/src/main/java/com/ohn/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.ohn.helloworld;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // xml 대신 코드로 구현 ==============================================
+//        LinearLayout mainLayout = new LinearLayout(this);
+//        mainLayout.setOrientation(LinearLayout.VERTICAL);
+//
+//        // 첫번째 변수: Width, 두번째 변수: height
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.WRAP_CONTENT
+//        );
+//
+//        Button button1 = new Button(this);
+//        button1.setText("Button1");
+//        button1.setLayoutParams(params);
+//        mainLayout.addView(button1);
+//
+//        setContentView(mainLayout); //보여주기 위해서 전달
+        // =================================================================
 
-        Log.d("Test", "Hello"); // 로그 출력하는 방법
+    }
+
+    public void onClickButton(View view) {
+        Log.d("Test", "Test");
     }
 }
