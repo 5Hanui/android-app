3727a09d6e6bdadafae5e5e9b5aa9c8c4adddf73
5Hanui <gkdud644@naver.com>
2021-07-05 13:41:00 +0900

    initial commit

---
+++ app/src/main/java/com/ohn/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ohn.helloworld;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("Test", "Hello"); // 로그 출력하는 방법
+    }
+}
